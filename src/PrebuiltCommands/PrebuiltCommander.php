50578ddaa9e90068ba55f63244bc8754ba5ae53e
Theodore R. Smith <theodore@phpexperts.pro>
2025-01-01 06:06:45 -0600

    Added a system for prebuilt commands.

---
+++ src/PrebuiltCommands/PrebuiltCommander.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minicli\PrebuiltCommands;
+
+use Minicli\App;
+
+class PrebuiltCommander
+{
+    public function __construct(App $app)
+    {
+
+    }
+}
